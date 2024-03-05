75c9105b791234153f0631f962a14ee9274624bd
GLODEIT\Vy.TK <vy.tk@globaldesignit.vn>
2024-03-05 11:21:22 +0700

    delete

--- core/Http/Bag/Cookies.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Core\Http\Bag;
-
-class Cookies extends Bag {
-    public function __construct() {
-        parent::__construct($_COOKIE);
-    }
-}
--- core/Http/Bag/Get.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Core\Http\Bag;
-
-class Get extends Bag {
-    public function __construct() {
-        parent::__construct($_GET);
-    }
-}
